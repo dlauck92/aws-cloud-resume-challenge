34ecc7cbc60029caf987e593f72fc11b259e799a
Drew Lauck <drewlauck92@gmail.com>
2025-01-06 16:47:11 -0500

    adding view counter logic and fetch lambda #'s

---
+++ index.js
@@ -0,0 +1,8 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter(){
+    let response = await fetch("https://bgc2fobyzfltb4qxrbqvxumge40lgppc.lambda-url.us-east-2.on.aws/")
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+updateCounter();
