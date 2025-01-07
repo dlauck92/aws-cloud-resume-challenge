f542c6dd19866c19f4cba40befd96e8dc78b89d2
Drew Lauck <drewlauck92@gmail.com>
2025-01-07 10:41:22 -0500

    adding counter div and updating s3 fetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter(){
-    let response = await fetch("https://bgc2fobyzfltb4qxrbqvxumge40lgppc.lambda-url.us-east-2.on.aws/")
+    let response = await fetch("https://bgc2fobyzfltb4qxrbqvxumge40lgppc.lambda-url.us-east-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
 }
